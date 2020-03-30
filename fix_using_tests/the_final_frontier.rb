5daa7d8ad219754755356ade98c9706f97059631
Alice Wang <aywang6@gmail.com>
2020-03-30 20:18:48 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -4,14 +4,17 @@ require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
+  binding.pry
 end
 
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
+  binding.pry
 end
 
 def engage
   puts state_log(date)
   date = generate_star_date
+  binding.pry
 end
